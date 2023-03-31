ac8db8d69e49da3a9cc46ef8711bb219054e6299
Jenbilyeu80 <jenbilyeu@yahoo.com>
2023-03-30 19:01:36 -0500

    First Studio

---
+++ src/FirstStudio/Circle.java
@@ -0,0 +1,11 @@
+package FirstStudio;
+//import java.util.Scanner;
+public class Circle {
+//    Scanner input = new Scanner(System.in);
+    public Circle(){
+
+    }
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+}
